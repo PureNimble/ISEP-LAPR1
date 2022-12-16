615cc79145f3c0f01654a685c00cbcb92d7141a3
Rafael Araújo <1201804@isep.ipp.pt>
2022-12-16 11:54:37 +0000

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,25 @@
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+public class Main {
+    public static void main(String[] args){
+		
+
+		try {
+			usingPrintWriter();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void usingPrintWriter() throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter(new File("src/teste.csv"));
+		StringBuffer csvHeader = new StringBuffer("");
+		csvHeader.append("Dia;S;I;R;N\n");
+		StringBuffer csvData = new StringBuffer("");
+		pw.write(csvHeader.toString());
+		csvData.append("John"+';'+"24"+';'+"Engineer"+'\n');
+		pw.write(csvData.toString());
+		pw.close();
+		}
 }
