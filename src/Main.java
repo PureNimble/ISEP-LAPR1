b29fc3997b4effcf126140c77c9ec52dd5f333e5
Rafael Araújo <1201804@isep.ipp.pt>
2022-12-16 12:53:03 +0000

    Update

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,25 @@ import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args){
 		
-
+		char m = args[0].charAt(0);	//1 e 2
+		float p = Float.parseFloat(args[1]);
+		int t = Integer.parseInt(args[2]);	//num de população
+		int d = Integer.parseInt(args[3]);	//num de dias
 		try {
-			usingPrintWriter();
+			usingPrintWriter(m, p, t, d);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	public static void usingPrintWriter() throws FileNotFoundException {
+	public static void usingPrintWriter(char m, float p, int t, int d) throws FileNotFoundException {
 		PrintWriter pw = new PrintWriter(new File("src/teste.csv"));
 		StringBuffer csvHeader = new StringBuffer("");
 		csvHeader.append("Dia;S;I;R;N\n");
 		StringBuffer csvData = new StringBuffer("");
 		pw.write(csvHeader.toString());
-		csvData.append("John"+';'+"24"+';'+"Engineer"+'\n');
+		csvData.append(m + ';'+ p +';'+ t + ';' + d + '\n');
 		pw.write(csvData.toString());
 		pw.close();
 		}
+	//compile by > javac nome do programa.java run by > java nome do ficheiro.java e parametros á frente (com espaços)
 }
