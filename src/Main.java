a38cb7ac77641780cf51baaad125987e6ab228a7
joaopinto15 <pintojad03@gmail.com>
2022-12-16 20:51:17 +0000

    testing

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,29 @@ import java.io.PrintWriter;
 public class Main {
     public static void main(String[] args){
 		/*
-		 * Estrutura do modo não interativo ->			java-jar nome programa.jar ficheiroSIR.csv-m X-p Y-t Z-d K ficheiroResultado.csv
+		 * Estrutura do modo não interativo ->			java -jar nome_programa.jar ficheiroSIR.csv -m X -p Y -t Z -d K ficheiroResultado.csv
 		 * 
-		 *	X -> Função      | (1-Euler,2-RK4)
-		 *  Y -> h           |condição (0<h<1)    
-         *  Z -> população(N)| 0 <  N
-		 *  K -> Número      | 0 < dias   
+		 *	-m -> Função         | (1-Euler,2-RK4)
+		 *  -p -> h              |condição (0<h<1)    
+         *  -t -> população(N)   | 0 <  N
+		 *  -d -> Número de dias | 0 < dias 
+		 * 
+		 * teste para os parametros -> java Main.java "1" "0.1" "1000" "10"  
 		 */
+	
 
-		char m = args[0].charAt(0);	//1 e 2
-		float p = Float.parseFloat(args[1]);
-		int t = Integer.parseInt(args[2]);	//num de população
-		int d = Integer.parseInt(args[3]);	//num de dias
+		char funcao = args[0].charAt(0);	//1 e 2
+		float h = Float.parseFloat(args[1]);
+		int N = Integer.parseInt(args[2]);	//num de população
+		int dias = Integer.parseInt(args[3]);	//num de dias
 		try {
-			usingPrintWriter(m, p, t, d);
+			usingPrintWriter(funcao, h, N, dias);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 	public static void usingPrintWriter(char m, float p, int t, int d) throws FileNotFoundException {
-		PrintWriter pw = new PrintWriter(new File("src/teste.csv"));
+		PrintWriter pw = new PrintWriter(new File("teste.csv"));
 		StringBuffer csvHeader = new StringBuffer("");
 		csvHeader.append("Dia;S;I;R;N\n");
 		StringBuffer csvData = new StringBuffer("");
