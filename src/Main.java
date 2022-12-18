dd19ef72735ea25b2180f855193c93c0fde4ee34
vscosousa <1221700@isep.ipp.pt>
2022-12-18 03:25:50 +0000

    estou a tentar

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,21 @@ import java.text.DecimalFormat;
 
 public class Main {
 	//Variaveis globais
-    public static float T = 1;
+    public static float T = 0;
     public static float N = 1000;
     public static float S = N - 1;
     public static float I = 1;
     public static float R = 0;
-	public static float Sn = N - 1;
+    public static float Sn = N - 1;
     public static float In = 1;
     public static float Rn = 0;
-    public static float taxaProp = 0.002f;           //β
-    public static float taxaPop = 0.6f;             //ρ
-    public static float taxaRej = 0.01f;           //γ
-    public static float taxaReI = 0.009f;         //α
+    public static float taxaProp = 0.002f;              //β
+    public static float taxaRej = 0.01f;                //γ
+    public static float taxaPop = 0.6f;                 //ρ
+    public static float taxaReI = 0;                    //α
     public static float h = 0.1f;
     public static void main(String[] args){
-		Euler();
+        Euler();
 		/*
 		 * Estrutura do modo não interativo ->			java -jar nome_programa.jar ficheiroSIR.csv -m X -p Y -t Z -d K ficheiroResultado.csv
 		 * 
@@ -179,7 +179,7 @@ public class Main {
 	 * @param float sucestíveis        										 *
 	 *************************************************************************/
 	public static float functionS(float T, float S){
-		return (-taxaProp * S * I);
+		return -taxaProp * S * I;
 	}
 	
 	/*************************************************************************
@@ -189,7 +189,7 @@ public class Main {
 	 * @param float I número de Infetados       							 *
 	 *************************************************************************/
 	public static float functionI(float T, float I){
-		return ((taxaPop * taxaProp * S * I) - (taxaRej * I) + (taxaReI * R));
+		return taxaPop * taxaProp * S * I - taxaRej * I + taxaReI * R;
 	}
 	
 	/*************************************************************************
@@ -199,7 +199,7 @@ public class Main {
 	 * @param float R número de recuperados        							 *
 	 *************************************************************************/
 	public static float functionR(float T, float R){
-		return ((taxaRej * I) - (taxaReI * R) + (1 - taxaPop) * (taxaProp * S * I));
+		return taxaRej * I - taxaReI * R + (1 - taxaPop) * taxaProp * S * I;
 	}
 	
 	/*************************************************************************
@@ -208,36 +208,37 @@ public class Main {
 	public static void Euler(){
 	DecimalFormat frmt = new DecimalFormat();
 	int i = 0;
-	int n = 3;
+	int n = 29;
 	float[][] resultados = new float[n][5];
 	System.out.println("Valor de S" + (i) + ": " + frmt.format(Sn));
 	System.out.println("Valor de I" + (i) + ": " + frmt.format(In));
 	System.out.println("Valor de R" + (i) + ": " + frmt.format(Rn));
 	System.out.println("Valor de N: " + (Sn + In + Rn));
 	while(i < n){
-			Sn = S + h * functionS(T + i * h, S);
-			S = Sn;
-			In = I + h * functionI(T + i * h, I);
-			I = In;
-			Rn = R + h * functionR(T + i * h, R);
-			R = Rn;
-			System.out.println("Valor de S" + (i) + ": " + frmt.format(Sn));
-			System.out.println("Valor de I" + (i) + ": " + frmt.format(In));
-			System.out.println("Valor de R" + (i) + ": " + frmt.format(Rn));
-			System.out.println("Valor de N: " + (Sn + In + Rn));
-			resultados[i][0] = i;
-			resultados[i][1] = Sn;
-			resultados[i][2] = In;
-			resultados[i][3] = Rn;
-			resultados[i][4] = Sn+In+Rn;
-			i++;
-		}
+		Sn = S + h * functionS((T + i * h), S);
+		S = Sn;
+		In = I + h * functionI((T + i * h), I);
+		I = In;
+		Rn = R + h * functionR((T + i * h), R);
+		R = Rn;
+		System.out.println("Valor de S" + (i) + ": " + frmt.format(Sn));
+		System.out.println("Valor de I" + (i) + ": " + frmt.format(In));
+		System.out.println("Valor de R" + (i) + ": " + frmt.format(Rn));
+		System.out.println("Valor de N: " + frmt.format(Sn + In + Rn));
 
-		try {
-			printFile("src/teste.csv", resultados, n);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		resultados[i][0] = i;
+		resultados[i][1] = Sn;
+		resultados[i][2] = In;
+		resultados[i][3] = Rn;
+		resultados[i][4] = Sn+In+Rn;
+		i++;
+	}
+
+	try {
+		printFile("src/teste.csv", resultados, n);
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
 	}
 
 	/*************************************************************************
@@ -246,55 +247,55 @@ public class Main {
 	public static void Runge_Kutta(){
 	DecimalFormat frmt = new DecimalFormat();
 	int i = 0;
-	int n = 3;
+	int n = 29;
 	float[][] resultados = new float[n][5];
 	System.out.println("Valor de S" + (i) + ": " + frmt.format(Sn));
 	System.out.println("Valor de I" + (i) + ": " + frmt.format(In));
 	System.out.println("Valor de R" + (i) + ": " + frmt.format(Rn));
-	System.out.println("Valor de N: " + (Sn + In + Rn));
+	System.out.println("Valor de N: " + frmt.format(Sn + In + Rn));
 	while(i < n){
-			float Sk1 = h * functionS(T,S);
-			float Sk2 = h * functionS(T + h/2, S + Sk1/2);
-			float Sk3 = h * functionS(T + h/2, S + Sk2/2);
-			float Sk4 = h * functionS(T + h, S + Sk3);
-			float Sk = (Sk1 + 2 * Sk2 + 2 * Sk3 + Sk4)/6;
-			Sn = S + Sk;
-			S = Sn;
-	
-			float Ik1 = h * functionI(T,I);
-			float Ik2 = h * functionI(T + h/2, I + Ik1/2);
-			float Ik3 = h * functionI(T + h/2, I + Ik2/2);
-			float Ik4 = h * functionI(T + h, I + Ik3);
-			float Ik = (Ik1 + 2 * Ik2 + 2 * Ik3 + Ik4)/6;
-			In = I + Ik;
-			I = In;
-	
-			float Rk1 = h * functionR(T,R);
-			float Rk2 = h * functionR(T + h/2, R + Rk1/2);
-			float Rk3 = h * functionR(T + h/2, R + Rk2/2);
-			float Rk4 = h * functionR(T + h, R + Rk3);
-			float Rk = (Rk1 + 2 * Rk2 + 2 * Rk3 + Rk4)/6;
-			Rn = R + Rk;
-			R = Rn;
+		float Sk1 = h * functionS(T,S);
+		float Sk2 = h * functionS((T + h/2), (S + Sk1/2));
+		float Sk3 = h * functionS((T + h/2), (S + Sk2/2));
+		float Sk4 = h * functionS((T + h), (S + Sk3));
+		float Sk = (Sk1 + 2 * Sk2 + 2 * Sk3 + Sk4)/6;
+		Sn = S + Sk;
+		S = Sn;
 
-			T = T + h;
-	
-			System.out.println("Valor de S" + (i) + ": " + frmt.format(Sn));
-			System.out.println("Valor de I" + (i) + ": " + frmt.format(In));
-			System.out.println("Valor de R" + (i) + ": " + frmt.format(Rn));
-			System.out.println("Valor de N: " + (Sn + In + Rn));
-			resultados[i][0] = i;
-			resultados[i][1] = Sn;
-			resultados[i][2] = In;
-			resultados[i][3] = Rn;
-			resultados[i][4] = Sn+In+Rn;
-			i++;
+		float Ik1 = h * functionI(T,I);
+		float Ik2 = h * functionI((T + h/2), (I + Ik1/2));
+		float Ik3 = h * functionI((T + h/2), (I + Ik2/2));
+		float Ik4 = h * functionI((T + h), (I + Ik3));
+		float Ik = (Ik1 + 2 * Ik2 + 2 * Ik3 + Ik4)/6;
+		In = I + Ik;
+		I = In;
+
+		float Rk1 = h * functionR(T,R);
+		float Rk2 = h * functionR((T + h/2), (R + Rk1/2));
+		float Rk3 = h * functionR((T + h/2), (R + Rk2/2));
+		float Rk4 = h * functionR((T + h), (R + Rk3));
+		float Rk = (Rk1 + 2 * Rk2 + 2 * Rk3 + Rk4)/6;
+		Rn = R + Rk;
+		R = Rn;
+		T += h;
+
+		System.out.println("Valor de S" + (i) + ": " + frmt.format(Sn));
+		System.out.println("Valor de I" + (i) + ": " + frmt.format(In));
+		System.out.println("Valor de R" + (i) + ": " + frmt.format(Rn));
+		System.out.println("Valor de N: " + frmt.format(Sn + In + Rn));
+
+		resultados[i][0] = i;
+		resultados[i][1] = Sn;
+		resultados[i][2] = In;
+		resultados[i][3] = Rn;
+		resultados[i][4] = Sn+In+Rn;
+		i++;
 		}
-		
-		try {
-			printFile("src/teste.csv", resultados, n);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+
+	try {
+		printFile("src/teste.csv", resultados, n);
+	} catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
 	}
 }
